89f73150a4e4b5c0470f75076a6c83c1ef424856
Jack Chakany <jack@chakany.systems>
2024-07-09 15:07:53 -0400

    nvim: remove galaxyline, move to ui.lua

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,22 @@
+return {
+  {
+      "rcarriga/nvim-notify",
+      init = function()
+          vim.notify = require("notify")
+      end,
+  },
+  {
+      "ellisonleao/gruvbox.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        contrast = "hard",
+      },
+      config = function(opts)
+        require("gruvbox").setup(opts)
+        vim.o.background = "dark"
+        vim.cmd([[colorscheme gruvbox]])
+      end,
+  },
+  "nvim-tree/nvim-web-devicons",
+}
